a58ee8f2f77af891ab42e42d749185e9e615b526
unknown <20253ds07@utez.edu.mx>
2026-03-04 09:24:09 -0600

    Recuperación U2: solución

--- Recupera_u2_2c/src/Recovery2CService.java
+++ Recupera_u2_2c/src/Recovery2CService.java
@@ -4,29 +4,46 @@ public class Recovery2CService {
         // TODO:
         // - null o en blanco -> false
         // - longitud exacta 7
+        if (codigo.length()>7 && codigo == null) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean esCategoriaValida(int categoria) {
+        if (categoria < 1 && categoria > 4) {
+            return true;
+        }
         return false;
     }
 
-    public static boolean esCategoriaValida(int categoria) {
-        // TODO: 1, 2, 3 o 4
-        return false;
-    }
 
     public static boolean esHoraValida(int horas) {
         // TODO: 1..12
-        return false;
+        if (horas > 12 && horas < 1) {
+            return false;
+        }
+        return true;
     }
+
 
     public static String clasificarPrioridad(int horas) {
         // TODO:
         // BAJA <= 3
         // MEDIA 4..7
         // ALTA > 7
+        if (horas <3){
+        }
+        if (horas <4 && horas >7){
+        }
         return "";
     }
 
     public static boolean requiereAutorizacion(int categoria, int horas) {
         // TODO: Laptop (1) y horas > 8
+        if (categoria == 1 && horas > 8) {
+            return true;
+        }
         return false;
     }
 }
