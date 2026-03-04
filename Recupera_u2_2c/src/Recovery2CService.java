9e600155dae75afd6d7a85bb64403af16070c282
gonzalezpaola <20253rd006@utez.edu.mx>
2026-03-04 09:09:38 -0600

    Recuperacion U2:solucion

--- Recupera_u2_2c/src/Recovery2CService.java
+++ Recupera_u2_2c/src/Recovery2CService.java
@@ -4,29 +4,47 @@ public class Recovery2CService {
         // TODO:
         // - null o en blanco -> false
         // - longitud exacta 7
+        if (codigo.equals("")) {
+            System.out.println("Vacio");
+        } else if(codigo.length()==7){
+            return true;
+        }
+
         return false;
     }
 
     public static boolean esCategoriaValida(int categoria) {
         // TODO: 1, 2, 3 o 4
+        if(categoria==1 || categoria==2 || categoria==3 || categoria==4){
+            return true;
+        }
         return false;
     }
 
     public static boolean esHoraValida(int horas) {
         // TODO: 1..12
+        if(horas>=1 && horas<=12){
+            return true;
+        }
         return false;
     }
 
     public static String clasificarPrioridad(int horas) {
-        // TODO:
-        // BAJA <= 3
-        // MEDIA 4..7
-        // ALTA > 7
+        if(horas<=3){
+            System.out.println("BAJA");
+        } else if (horas>=4 && horas<=7){
+            System.out.println("MEDIA");
+        }else{
+            System.out.println("ALTA");
+        }
         return "";
     }
 
     public static boolean requiereAutorizacion(int categoria, int horas) {
         // TODO: Laptop (1) y horas > 8
+        if(categoria==1 && horas>8){
+            return true;
+        }
         return false;
     }
 }
